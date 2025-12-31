bd7ab5e26486d90b3607d25d95b5e3b414a57752
hellonagi <38994662+hellonagi@users.noreply.github.com>
2025-12-31 23:32:20 +0900

    Fix fallback urls

--- apps/web/lib/api.ts
+++ apps/web/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || '';
 
 export const api = axios.create({
   baseURL: `${BASE_URL}/api`,
